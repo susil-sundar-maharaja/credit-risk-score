2e16b1f301bd5a196ab4877f825aac523378b172
susil-sundar-maharaja <susilsundarmaharaja@gmail.com>
2026-07-10 02:49:34 +0530

    Fix model path

--- app/prediction_helper.py
+++ app/prediction_helper.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-MODEL_PATH = 'artifacts/model_data.joblib'
+MODEL_PATH = 'app/artifacts/model_data.joblib'
 
 model_data = joblib.load(MODEL_PATH)
 model = model_data['model']
